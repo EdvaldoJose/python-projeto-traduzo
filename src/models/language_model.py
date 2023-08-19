e9a141adb53493f7eaad0a5682d75b9166bc33b7
Edvaldo José <edvaldojose003@gmail.com>
2023-08-19 08:04:34 -0300

    feat: Req. 1 - Instanciando idiomas

--- src/models/language_model.py
+++ src/models/language_model.py
@@ -1,11 +1,13 @@
-# from .abstract_model import AbstractModel
-# from database.db import db
+from .abstract_model import AbstractModel
+from database.db import db
 
 
 # Req. 1
-class LanguageModel:
-    def __init__(self):
-        raise NotImplementedError
+class LanguageModel(AbstractModel):
+    _collection = db['languages']
+
+    def __init__(self, data: dict = {}):
+        self.data = data
 
     # Req. 2
     def to_dict(self):
