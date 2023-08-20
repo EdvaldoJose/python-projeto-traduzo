1f3a1ab43abbe415e5718cb1d7cd8487f7c5b42c
Edvaldo José <edvaldojose003@gmail.com>
2023-08-19 21:47:37 -0300

    feat: Req. 7 TESTE - Histórico de Traduções

--- tests/models/history/test_history_model.py
+++ tests/models/history/test_history_model.py
@@ -1,7 +1,18 @@
 # import json
 # from src.models.history_model import HistoryModel
+import json
+from src.models.history_model import HistoryModel
 
 
 # Req. 7
 def test_request_history():
-    raise NotImplementedError
+    json_data = HistoryModel.list_as_json()
+    data = json.loads(json_data)
+
+    assert len(data) == 2
+    assert data[0]["text_to_translate"] == "Hello, I like videogame"
+    assert data[0]["translate_from"] == "en"
+    assert data[0]["translate_to"] == "pt"
+    assert data[1]["text_to_translate"] == "Do you love music?"
+    assert data[1]["translate_from"] == "en"
+    assert data[1]["translate_to"] == "pt"
